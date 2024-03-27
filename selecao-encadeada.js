abdc4160d2406c2175e9265f2fce9b11be4f59cd
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 16:01:52 -0400

    adicionado exemplo de estrutura de seleção encadeada

---
+++ selecao-encadeada.js
@@ -0,0 +1,10 @@
+let dia = "Domingão"
+
+if (dia === "Sabadão") {
+    console.log("meta semanal cumprida, descanse escravo")
+} else if (dia === "Domingão") {
+console.log("KKKKKKKKKKKKKKKKKKKKKKKKK oc trabaia amanhã otaro") 
+} else {
+    console.log("bora bora bora cabo o intervalo")
+}
+
