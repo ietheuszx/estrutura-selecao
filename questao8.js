b84478d961011ceba4bda5db3e7d4bb5aebec85d
ietheuszx <fernandomaranss68@gmail.com>
2024-04-01 17:03:27 -0400

    atividade realizada

---
+++ questao8.js
@@ -0,0 +1,16 @@
+/*
+Questão 8: Como você pode usar uma declaração if, else if, else para 
+determinar se uma idade corresponde a bebê, criança, adolescente, adulto ou 
+idoso em JavaScript?
+*/
+
+let numero = 10
+if (numero > 3) {
+   console.log("Kid")
+} else if (numero > 1) {
+   console.log("Baby")
+} else {
+   console.log("veio")
+}if ( 10 < numero ) {
+    console.log(" é criança")
+}
